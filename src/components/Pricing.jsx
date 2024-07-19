c63836e7a76d6449c1f20de9e88600a304547858
Danny <dannydotdev@gmail.com>
2024-07-19 15:17:07 +0100

    Roadmap

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -40,6 +40,15 @@ const Pricing = () => {
           <LeftLine />
           <RightLine />
         </div>
+
+        <div className="flex justify-center  mt-10">
+          <a
+            href="/pricing"
+            className="font-bold font-code text-xs tracking-wider uppercase border-b"
+          >
+            See the full details
+          </a>
+        </div>
       </div>
     </Section>
   );
